873018b02ed6466d594a5cab2f717eafbcfc1232
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 01:39:41 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,7 @@
+def prime?(num)
+   return false if num < 2
+   range = 2..num-1 #num-1: if num doesn't divide anything less than it => true
+   range.each do |x|
+      return false if num % x == 0
+   end
+   true
