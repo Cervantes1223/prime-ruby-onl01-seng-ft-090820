94c08a3784ae714590c43afa47b718d6508d85bb
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 01:40:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+   return false if num < 2
+   range = 2..num-1 #num-1: if num doesn't divide anything less than it => true
+   range.each do |x|
+      return false if num % x == 0
+   end
+   true
+ end
